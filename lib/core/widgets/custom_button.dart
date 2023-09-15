4b65f8526e1d96dda52e10894fe593b3f862d0ed
MoaazAbdelaziz <moaazwork1@gmail.com>
2023-09-15 21:33:15 +0300

    create books action

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,42 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.backGroundColor,
+    required this.text,
+    this.borderRadius,
+    required this.textColor,
+    this.fontSize,
+  });
+  final Color backGroundColor;
+  final Color textColor;
+  final String text;
+  final BorderRadius? borderRadius;
+  final double? fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: backGroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ?? BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(
+          text,
+          style: Styles.textStyle18.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900,
+            fontSize: fontSize,
+          ),
+        ),
+      ),
+    );
+  }
+}
